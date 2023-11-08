455aeeb0433666da5ee2a3e4da450d33ec262641
Vovabah56 <vovabah56@yandex.ru>
2023-11-08 19:23:07 +0700

    контроллер заказа

---
+++ Delivery/Controllers/OrderController.cs
@@ -0,0 +1,28 @@
+﻿using Delivery.DB;
+using Delivery.DTO;
+using Delivery.Services.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace Delivery.Controllers;
+
+[ApiController]
+[Route("api/order")]
+public class OrderController: ControllerBase
+{
+    public IOrderService _orderService { get; set; }
+
+    public OrderController(IOrderService orderService)
+    {
+        _orderService = orderService;
+    }
+    
+    [HttpPost]
+    [Authorize]
+    [Authorize(Policy = "ValidateToken")]
+    public async Task CreateOrder([FromBody] CreateOrderDto orderCreateDto)
+    {
+        await _orderService.CreateOrder(Guid.Parse(User.Identity.Name), orderCreateDto);
+    }
+}
